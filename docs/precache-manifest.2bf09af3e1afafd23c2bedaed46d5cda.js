b292a56e6c7ab2b2ac8afc387f695ce0aa6b8c8a
Dariusz Futera <df@zycio.pl>
2020-09-03 12:15:29 +0200

    Added docs folder.

---
+++ docs/precache-manifest.2bf09af3e1afafd23c2bedaed46d5cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8407fb82e28ae4d10b8d8fc5ed420e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d1fb38b512dbd58948ca",
+    "url": "./static/css/main.3efbc168.chunk.css"
+  },
+  {
+    "revision": "7a5fe934e20789af9071",
+    "url": "./static/js/2.2ef5f318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2ef5f318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1fb38b512dbd58948ca",
+    "url": "./static/js/main.4e595726.chunk.js"
+  },
+  {
+    "revision": "a88fbb2a73fabbeab46b",
+    "url": "./static/js/runtime-main.0124c5e7.js"
+  }
+]);
